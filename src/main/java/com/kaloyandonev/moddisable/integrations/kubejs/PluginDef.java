41519f71af8504c5dc9f8e00b16b2cfd24f12fbb
Kaloyan Donev <donevkaloyan@gmail.com>
2025-10-16 17:02:48 +0300

    I think this adds the full KubeJS integration. Needs testing.

--- src/main/java/com/kaloyandonev/moddisable/integrations/kubejs/PluginDef.java
+++ src/main/java/com/kaloyandonev/moddisable/integrations/kubejs/PluginDef.java
@@ -6,6 +6,6 @@ import dev.latvian.mods.kubejs.script.BindingRegistry;
 public class PluginDef implements KubeJSPlugin {
     @Override
     public void registerBindings(BindingRegistry registry){
-        registry.add("ModDisable", KubeJSItemManager.class);
+        registry.add("ModDisable", new KubeJSItemManager());
     }
 }
