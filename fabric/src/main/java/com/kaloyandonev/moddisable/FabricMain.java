cbfa2240688893b13d24067f897e27df10068033
Kaloyan Donev <donevkaloyan@gmail.com>
2025-12-03 03:08:32 +0200

    Reimplement NeoMain:ClientCode into Fabric.

--- fabric/src/main/java/com/kaloyandonev/moddisable/FabricMain.java
+++ fabric/src/main/java/com/kaloyandonev/moddisable/FabricMain.java
@@ -1,7 +1,13 @@
 package com.kaloyandonev.moddisable;
 
 import com.kaloyandonev.moddisable.events.ModEvents;
+import com.kaloyandonev.moddisable.migrators.pre_1_1_0_migrator.ScreenCrator;
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 
 public class FabricMain implements ModInitializer {
     
@@ -17,4 +23,21 @@ public class FabricMain implements ModInitializer {
         CommonClass.init();
         ModEvents.onCommandRegister();
     }
+
+    //TO DO: ADD LISTENER
+    private void ClientCode(){
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            Minecraft.getInstance().execute(() -> {
+                ScreenCrator screenCrator = new ScreenCrator(
+                        Component.literal("Migration Title"), // Title of the screen
+                        Component.literal("Description of the screen"), // Description
+                        confirmed -> false
+                );
+
+                // Open the new screen
+                Minecraft.getInstance().setScreen(screenCrator);
+            });
+        }
+    }
+
 }
